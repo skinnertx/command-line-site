c676a64d9e83b245442eb306d4f89194e893cb1b
skinnertx <65523964+skinnertx@users.noreply.github.com>
2023-11-20 08:00:44 -0600

    Added command support
    began implementing commands for the terminal like: help, clear, and whoami

--- src/pages/CommandInterpreter.js
+++ src/pages/CommandInterpreter.js
@@ -1,24 +1,63 @@
+import styles from '@/styles/Page.module.css';
+
+const funcList = [
+    'help -> print a list of all available commands',
+    'whoami -> display information about the creator of this site',
 
 
-export default function Interpreter({command}) {
-    console.log(command);
-    switch(command) {
+    'clear -> clear the terminal (except for intro text)'
+]
+
+export default function Interpreter(command) {
+    const lowCommand = command.toLowerCase(); // used for case matching
+    // pass command actual to return what user ACTUALLY typed
+    switch(lowCommand) {
         case 'help':
-            return help();
-        
+            return help(command);
+
+        case 'whoami':
+            return whoami(command);
+
         default:
-            return invalid();
+            return invalid(command);
     }
 }
 
-function help() {
+function help(actual) {
     return (
-        'here we go gamer'
+        <span>
+            {actual}
+            <pre className={styles.indent}>
+            {funcList.map(item => (
+                <span>{'\t'}{item}<br/></span>
+            ))}
+            </pre>
+        </span>
+        
     )
 }
 
-function invalid() {
+function whoami(actual) {
     return (
-        'not a valid command'
+        <span>
+            {actual}
+            <pre className={styles.indent}>
+                {'\t'} Hello there!<br/>
+            </pre>
+        </span>
     )
 }
+
+
+
+function invalid(actual) {
+    return (
+        <span>
+            {actual}
+            <pre className={styles.indent}>
+            {'\t'}not a valid command
+            </pre>
+        </span>
+    )
+}
+
--- src/pages/CommandLine.js
+++ src/pages/CommandLine.js
@@ -4,6 +4,9 @@ import Interpreter from './CommandInterpreter';
 import styles from '@/styles/Page.module.css';
 import { useEffect, useRef, useState } from "react";
 
+// animation scaler - used to speed up animations when debugging
+const aniScale = 0.2;
+
 {/* 
     if this prefix is changed, min width for .inputPrefix must be adjusted
     inside styles/Page.module.css
@@ -51,12 +54,16 @@ function History() {
     const inputRef = useRef(null);
     const bottomRef = useRef(null);
     const initRef = useRef(null);
+    const introConRef = useRef(null);
 
     // adds whatever item is in newValue to the history list
-    function addItem() {
+    function addItem(value=null) {
+        if(value == null) {
+            return;
+        }
         const newHistory = [
             ...history,
-            {id: history.length, value: newValue}
+            {id: history.length, value: value}
         ];
         updateHistory(newHistory);
     }
@@ -65,10 +72,19 @@ function History() {
     // TODO: figure out why command is invalid
     function handleKeyDown(e) {
         if(e.key === 'Enter') {
-            addItem();
+            //addItem();
             inputRef.current.value = '';
-            const result = Interpreter(newValue.toLowerCase());
-            console.log(result);
+            if (newValue.toLowerCase() === 'clear') {
+                updateHistory([]);
+                // im aware this is stupid, but alas, tech debt of how i designed the animation makes me do this
+                introConRef.current.style.position = 'absolute';
+                introConRef.current.style.top = '-100vh';
+            } else {
+                const result = Interpreter(newValue);
+                addItem(result);
+                console.log(history);
+            }
+
         }
         bottomRef.current.scrollIntoView();
     }
@@ -76,19 +92,19 @@ function History() {
     // set css after time for certain elements (animation)
     useEffect(() => {
         // set visibility of init animation
-        setTimeout(() => {initRef.current.style.visibility = 'visible';}, 3600);
+        setTimeout(() => {initRef.current.style.visibility = 'visible';}, 3600 * aniScale);
         // set visibility of input component
-        setTimeout(() => {inputConRef.current.style.visibility = 'visible';}, 8200);
+        setTimeout(() => {inputConRef.current.style.visibility = 'visible';}, 8200 * aniScale);
     })
 
     return (
         <div className={styles.history}>
-            <div>
-                <Prefix/><TypedLine startDelay={3000} typeSpeed={15} toPrint={['init sisyphus.exe']}/><br/>
+            <div ref={introConRef}>
+                <Prefix/><TypedLine startDelay={3000 * aniScale} typeSpeed={15} toPrint={['init sisyphus.exe']}/><br/>
                 <pre className={styles.tab} ref={initRef}>
-                    {'\t'}<TypedLine typeSpeed={25} startDelay={3200} toPrint={['- Rolling boulder uphill']}/><br/>
-                    {'\t'}<TypedLine typeSpeed={10} startDelay={4700} toPrint={['- Finished in 280ms, initiating rollback']}/><TypedLine typeSpeed={5} loopCount={3} loop={true} startDelay={6000} toPrint={['. . .']}/><br/>
-                    {'\t'}<TypedLine typeSpeed={1} startDelay={7800} toPrint={['- Done, type HELP for a list of commands']}/><br/>
+                    {'\t'}<TypedLine typeSpeed={25} startDelay={3200 * aniScale} toPrint={['- Rolling boulder uphill']}/><br/>
+                    {'\t'}<TypedLine typeSpeed={10} startDelay={4700 * aniScale} toPrint={['- Finished in 280ms, initiating rollback']}/><TypedLine typeSpeed={5} loopCount={3} loop={true} startDelay={6000} toPrint={['. . .']}/><br/>
+                    {'\t'}<TypedLine typeSpeed={1} startDelay={7800 * aniScale} toPrint={['- Done, type <u>HELP</u> for a list of commands']}/><br/>
                 </pre>
             </div>
             {history.map(item => (
